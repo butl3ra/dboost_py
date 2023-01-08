33af2ee536f61d57c36f7f50c81ac8dc393c8e21
butl3ra <andrew.butler.d@gmail.com>
2023-01-08 20:26:43 -0330

    loss sse added to dboost loss for boosting

--- dboost_py/loss.py
+++ dboost_py/loss.py
@@ -1,5 +1,6 @@
 import numpy as np
 from dboost_py.utils import quad_form
+from maple.loss import loss_sse
 
 
 def loss_spo(y, y_hat, oracle):
@@ -12,6 +13,22 @@ def loss_spo(y, y_hat, oracle):
         raise Exception("y_hat is dict but y is not. Both must be dict or array.")
     elif is_y_dict and is_y_hat_dict:
         loss = loss_cart_spo(y=y, y_hat=y_hat, oracle=oracle)
+    else:
+        loss = loss_spo_core(y=y, y_hat=y_hat, oracle=oracle)
+
+    return loss
+
+
+def loss_dboost_spo(y, y_hat, oracle):
+    # --- type checking
+    is_y_dict = isinstance(y, dict)
+    is_y_hat_dict = isinstance(y_hat, dict)
+    if is_y_dict and not is_y_hat_dict:
+        raise Exception("y is dict but y_hat is not. Both must be dict or array.")
+    elif not is_y_dict and is_y_hat_dict:
+        raise Exception("y_hat is dict but y is not. Both must be dict or array.")
+    elif is_y_dict and is_y_hat_dict:
+        loss = loss_sse(y=y, y_hat=y_hat)
     else:
         loss = loss_spo_core(y=y, y_hat=y_hat, oracle=oracle)
 
@@ -49,6 +66,7 @@ def loss_spo_core(y, y_hat, oracle):
 
 
 # --- does not yet support option for a P matrix that is different than oracle.P
+# --- get_P_eval; init with P_eval and get oracle.P_eval if it exists...
 def loss_qspo(y, y_hat, oracle):
     # --- type checking
     is_y_dict = isinstance(y, dict)
@@ -59,6 +77,22 @@ def loss_qspo(y, y_hat, oracle):
         raise Exception("y_hat is dict but y is not. Both must be dict or array.")
     elif is_y_dict and is_y_hat_dict:
         loss = loss_cart_qspo(y=y, y_hat=y_hat, oracle=oracle)
+    else:
+        loss = loss_qspo_core(y=y, y_hat=y_hat, oracle=oracle)
+
+    return loss
+
+
+def loss_dboost_qspo(y, y_hat, oracle):
+    # --- type checking
+    is_y_dict = isinstance(y, dict)
+    is_y_hat_dict = isinstance(y_hat, dict)
+    if is_y_dict and not is_y_hat_dict:
+        raise Exception("y is dict but y_hat is not. Both must be dict or array.")
+    elif not is_y_dict and is_y_hat_dict:
+        raise Exception("y_hat is dict but y is not. Both must be dict or array.")
+    elif is_y_dict and is_y_hat_dict:
+        loss = loss_sse(y=y, y_hat=y_hat)
     else:
         loss = loss_qspo_core(y=y, y_hat=y_hat, oracle=oracle)
 
